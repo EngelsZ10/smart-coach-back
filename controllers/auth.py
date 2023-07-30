67fbec81f334f54627e125d2b167b3cdc73c9433
EngelsZ10 <62487204+EngelsZ10@users.noreply.github.com>
2023-07-29 18:09:54 -0600

    acctualizar

--- controllers/auth.py
+++ controllers/auth.py
@@ -2,16 +2,16 @@ from ftplib import FTP
 from flask import request, jsonify
 
 def login():
-    user = request.args.get("user")
+    email = request.args.get("email")
     pas = request.args.get("pass")
     ftp = FTP("ftp.smartcoach.top")
     try:
-        r = ftp.login(f"{user}@smartcoach.top", pas)
+        r = ftp.login(f"{email}", pas)
     except Exception as e:
         r = e
     resp = {
         "response": True if r.__str__()[:3] == '230' else False,
-        "Admin": True if r.__str__()[:3] == '230' and user == "df3artco4sf" else False,
+        "Admin": True if r.__str__()[:3] == '230' and email == "df3artco4sf" else False,
         "mensaje": r.__str__()
     }
     ftp.quit()
